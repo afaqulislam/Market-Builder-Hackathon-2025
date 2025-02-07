422bbf9c82f93b5999b4b0b9c99384d91e70a708
afaqulislam <afaqulislam707@gmail.com>
2025-02-08 00:59:53 +0500

    AUI | Rental E-Commerce

--- nextjs-hackathone-project/app/components/Navbar.tsx
+++ nextjs-hackathone-project/app/components/Navbar.tsx
@@ -6,10 +6,11 @@ import { useState } from "react";
 import Link from "next/link";
 import { CiSliderHorizontal, CiSearch } from "react-icons/ci";
 import { FaHeart, FaBell, FaCog, FaBars, FaUser } from "react-icons/fa";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isSignedIn } = useUser();
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-white/30 backdrop-blur-md border border-white/20 rounded-lg shadow-md">
@@ -28,11 +29,11 @@ const Navbar: React.FC = () => {
           {/* Search Bar - Hidden on mobile, visible on larger screens */}
           <div className="hidden md:flex items-center flex-1 max-w-4xl mx-4">
             <div className="w-full">
-              <div className="relative ">
+              <div className="relative">
                 <input
                   type="text"
                   placeholder="Search something here"
-                  className="w-full pl-10  pr-4 py-2 rounded-full border focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full pl-10 pr-4 py-2 rounded-full border focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <CiSearch className="h-5 w-5 text-gray-400" />
@@ -60,7 +61,7 @@ const Navbar: React.FC = () => {
                 aria-label="Notifications"
               >
                 <FaBell className="h-6 w-6" />
-              </button>{" "}
+              </button>
             </Link>
             <Link href="/settings" passHref>
               <button
@@ -71,14 +72,13 @@ const Navbar: React.FC = () => {
               </button>
             </Link>
             <div className="ml-4 flex-shrink-0">
-              <SignedOut>
+              {isSignedIn ? (
+                <UserButton />
+              ) : (
                 <SignInButton>
                   <FaUser className="h-6 w-6 text-gray-500 hover:text-blue-600 cursor-pointer" />
                 </SignInButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
+              )}
             </div>
           </div>
 
@@ -96,7 +96,7 @@ const Navbar: React.FC = () => {
         </div>
       </div>
 
-      {/* Mobile menu, show/hide based on menu state */}
+      {/* Mobile menu */}
       {isMenuOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
@@ -114,34 +114,30 @@ const Navbar: React.FC = () => {
               passHref
               className="text-gray-600 hover:text-blue-600 flex px-3 py-2 rounded-md text-base font-medium"
             >
-              <FaHeart className="h-6 w-6 mr-3" />
-              Favorites
+              <FaHeart className="h-6 w-6 mr-3" /> Favorites
             </Link>
             <Link
               href="/notifications"
               passHref
               className="text-gray-600 hover:text-blue-600 flex px-3 py-2 rounded-md text-base font-medium"
             >
-              <FaBell className="h-6 w-6 mr-3" />
-              Notifications
+              <FaBell className="h-6 w-6 mr-3" /> Notifications
             </Link>
             <Link
               href="/settings"
               passHref
               className="text-gray-600 hover:text-blue-600 flex px-3 py-2 rounded-md text-base font-medium"
             >
-              <FaCog className="h-6 w-6 mr-3" />
-              Settings
+              <FaCog className="h-6 w-6 mr-3" /> Settings
             </Link>
             <div className="flex items-center px-3 py-2">
-              <SignedOut>
+              {isSignedIn ? (
+                <UserButton />
+              ) : (
                 <SignInButton>
                   <FaUser className="h-6 w-6 text-gray-500 hover:text-blue-600 cursor-pointer" />
                 </SignInButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
+              )}
               <span className="text-gray-600 hover:text-blue-600 mx-2">
                 Your Profile
               </span>
