37c5c32a9e26635efb476861400cfdc32b468123
Afaq ul Islam <afaqulislam707@gmail.com>
2025-02-09 19:09:32 +0500

    Delete src directory

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import banner from '../schemas/banner'
-import products from './products'
-import category from './category'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [banner, products, category],
-}
